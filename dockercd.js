7a72c9d682d520c8e07ecc98e73b770344a61dda
srutner <srutner@credomobile.com>
2017-05-03 15:34:24 -0700

    add gitignore

--- dockercd.js
+++ dockercd.js
@@ -26,6 +26,7 @@ function registerJob({ configJson, jobName }, { dockerJSON }, callback) {
   const pushed_at = dockerJSON.push_data.pushed_at.toString();
   configJson.Job.TaskGroups[0].Tasks[0].Config.image = `${owner}` + "/" + `${jobName}` + ":" + `${tag}`;
   configJson.Job.TaskGroups[0].Tasks[0].Env.DOCKER_BUILT_AT = dockerJSON.push_data.pushed_at.toString();
+  configJson.Job.Meta.buildnumber = tag
   request({
     uri: `${URL_BASE}${jobName}`,
     method: 'POST',
@@ -50,5 +51,5 @@ exports.handler = (event, context, callback) => {
       if (err) return callback(err);
       callback(null, response);
     });
-  }); 
+  });
 };
